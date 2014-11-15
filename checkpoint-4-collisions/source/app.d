f3e934b1b5244df1ebc6ec20982edb80590fce6c
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-11-15 19:14:08 +0100

    Licenses.

--- checkpoint-4-collisions/source/app.d
+++ checkpoint-4-collisions/source/app.d
@@ -1,3 +1,8 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 import std.stdio;
 import std.algorithm: canFind, count, filter, min, remove; // (1)
 import std.math: fmod, PI; // (2)
